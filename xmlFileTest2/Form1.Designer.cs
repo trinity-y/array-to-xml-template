68817a785d10fd044dfed8f481f981a86ca1044c
unknown <trinity-y>
2020-03-25 00:03:02 -0400

    added project

---
+++ xmlFileTest2/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace xmlFileTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(67, 93);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(113, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "SAVE BY LOOP";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 220);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ xmlFileTest2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace xmlFileTest2
+{
+    public partial class Form1 : Form
+    {
+        Decimal[] array1 = new Decimal[4] { 0.1m, 93.2m, 3876.1839m, 12.3892m };
+        Decimal[] array2 = new Decimal[4] { 0.1m, 93.2m, 3876.1839m, 12.3892m };
+        Decimal[] array3 = new Decimal[4] { 0.1m, 93.2m, 3876.1839m, 12.3892m };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //creates an empty sort of 'frame' for the data
+            String xmlBase =
+                @"<?xml version=""1.0""?> 
+                <root>
+                    <array1>
+                    </array1>
+                    <array2>
+                    </array2>
+                    <array3>
+                    </array3>
+                </root>";
+            //parses the string frame into an xml file
+            XDocument savedPiece = XDocument.Parse(xmlBase);
+
+            //each foreach loop fills in the node with data from an array
+            foreach (Decimal element in array1)
+            {
+                savedPiece.Element("root").Element("array1").Add(new XElement("queue", element));
+            }
+            foreach (Decimal element in array2)
+            {
+                savedPiece.Element("root").Element("array2").Add(new XElement("queue", element));
+            }
+            foreach (Decimal element in array3)
+            {
+                savedPiece.Element("root").Element("array3").Add(new XElement("queue", element));
+            }
+            //saves it to computer with name
+            string path = System.IO.Path.GetDirectoryName(Application.ExecutablePath) + "\\savedpiece.xml";
+            savedPiece.Save(path);
+
+        }
+    }
+}
